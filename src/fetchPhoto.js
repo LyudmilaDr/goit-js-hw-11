a6759e524530a7f20b5b3e723f35c50579e48583
LyudmilaDr <ludralo44@gmail.com>
2023-02-26 00:11:19 +0200

    add src

--- src/fetchPhoto.js
+++ src/fetchPhoto.js
@@ -1,9 +1,9 @@
-// export const fetchPhoto = async(query, page)=>{
-//     return await fetch(`https://pixabay.com/api/?key=33899578-833a0699edaa5282298dabcf2&q=${query}&image_type="photo" &orientation="horizontal" &safesearch="true"&page=${page}&per_page=40`)
-//     .then(async response => {
-// if(!response.ok){
-//     throw new Error(response.statusText);
-// }
-// return await response.json();
-//     })
-// };
+import axios from 'axios';
+export const fetchPhoto = async(query, page = 1)=>{
+    try {
+    const response = await axios.get(`https://pixabay.com/api/?key=33899578-833a0699edaa5282298dabcf2&q=${query}&image_type="photo"&orientation="horizontal"&safesearch="true"&page=${page}&per_page=40`)
+    return response.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,96 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import axios from 'axios';
 import { fetchPhoto } from './fetchPhoto';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
+const Photogallery = document.querySelector('.gallery');
+const searchForm = document.querySelector('#search-form');
+const btnLoadMore = document.querySelector('.load-more');
 
-// const inputEl = document.querySelector('.form-input');
-// const button = document.querySelector('.form-btn');
-// let page = 1;
+btnLoadMore.style.display = 'none';
+let page = 1;
+let query = '';
 
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 
-
-/* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div> */
+  const onClickSubmit = e => {
+    e.preventDefault();
+    query = searchForm.searchQuery.value;
+    if (!query) {
+    btnLoadMore.style.display = 'none';
+    Photogallery.innerHTML = '';
+      return;
+    }
+   fetchPhoto(query)
+   .then(data => {
+      if (data.hits.length === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.'
+        );
+        Photogallery.innerHTML = '';
+        return;
+      } else if (data.totalHits > 40) {
+        btnLoadMore.style.display = 'flex';
+      }
+      page = 1;
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      Photogallery.innerHTML = galleryMarkup(data.hits);
+      gallery.refresh();
+    });
+  };
+  const onClickLoadMore = () => {
+    page += 1;
+    fetchPhoto(query, page).then(data => {
+        Photogallery.insertAdjacentHTML('beforeend',  galleryMarkup(data.hits));
+      gallery.refresh();
+      if (page >= Math.ceil(data.totalHits / 40)) {
+        page = 1;
+        btnLoadMore.style.display = 'flex';
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    });
+  };
+  
+  function galleryMarkup(arr) {
+    return arr
+      .map(({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) =>
+          `<div class="photo-card" >
+              <a href=${largeImageURL}><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+              <div class="info">
+              <p class="info-item">
+                  <b>Likes:
+                  ${likes}</b>
+              </p>
+              <p class="info-item">
+                  <b>Views:
+                  ${views}</b>
+              </p>
+              <p class="info-item">
+                  <b>Comments:
+                  ${comments}</b>
+              </p>
+              <p class="info-item">
+                  <b>Downloads:
+                  ${downloads}</b>
+              </p>
+              </div>
+        </div>`
+      )
+      .join('');
+  }
+  searchForm.addEventListener('submit', onClickSubmit);
+  btnLoadMore.addEventListener('click', onClickLoadMore);
